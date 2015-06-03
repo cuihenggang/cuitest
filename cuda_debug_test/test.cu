a7cfb185a3659f9d20abc5452abb22eb8e0f41ff
Henggang Cui at susitna <hengganc@h0.ubuntu14-tmp.biglearning.susitna.pdl.cmu.local>
2015-06-03 12:20:15 -0400

    making the bug more interesting

--- cuda_debug_test/test.cu
+++ cuda_debug_test/test.cu
@@ -7,20 +7,17 @@
 using namespace std;
 
 int main() {
-  size_t count = 1000 * 20000;
+  size_t count = 20 * 1000 * 1000;
   size_t size = count * sizeof(float);
   void *arrays[10];
   for (size_t i = 0; i < 10; i++) {
     assert(cudaMalloc(&arrays[i], size) == cudaSuccess);
-    assert(cudaMemset(arrays[i], 0, size) == cudaSuccess);
   }
   void *host_array;
   assert(cudaMallocHost(&host_array, size) == cudaSuccess);
-  for (size_t i = 1; i < 10; i++) {
-    void *dst_array = arrays[i];
-    void *src_array = arrays[0];
-    assert(cudaMemcpy(dst_array, src_array, size, cudaMemcpyDeviceToDevice) == cudaSuccess);
-    cout << "copied" << i << endl;
+  for (size_t i = 0; i < 10; i++) {
+    assert(cudaMemset(arrays[8], 0, size) == cudaSuccess);
+    cout << "memset" << i << endl;
     for (size_t j = 0; j < 10; j++) {
       void *array = arrays[j];
       assert(cudaMemcpy(host_array, array, size, cudaMemcpyDeviceToHost) == cudaSuccess);
