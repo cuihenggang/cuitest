96ad2158cdb9cc27ad5301684b217739b3d2f5fb
Henggang Cui at susitna <hengganc@h0.centos-2.biglearning.susitna.pdl.cmu.local>
2015-11-03 14:02:07 -0700

    gdr test finished

--- gdr_test/test.cpp
+++ gdr_test/test.cpp
@@ -19,32 +19,41 @@ using namespace std;
 
 int main (int argc, char *argv[])
 {
-  int myid, numprocs, i;
-  int size;
+  int myid;
+  int numprocs;
+  int size = 1 << 29;   /* 512 MB */
+  int rounds = 10;
   MPI_Status reqstat;
-  char *s_buf, *r_buf;
+  char *s_buf;
+  char *r_buf;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-  double t_start = 0.0, t_end = 0.0;
-
-  size = 1 << 22;
   CUDA_CHECK(cudaMalloc(&s_buf, size));
   CUDA_CHECK(cudaMalloc(&r_buf, size));
 
   MPI_Barrier(MPI_COMM_WORLD);
+  double t_start = MPI_Wtime();
 
-  if(myid == 0) {
-    MPI_Send(s_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-    MPI_Recv(r_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &reqstat);
+  if (myid == 0) {
+    for (int r = 0; r < rounds; r++) {
+      MPI_Send(s_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+      MPI_Recv(r_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &reqstat);
+    }
   }
 
-  else if(myid == 1) {
-    MPI_Recv(r_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &reqstat);
-    MPI_Send(s_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+  else if (myid == 1) {
+    for (int r = 0; r < rounds; r++) {
+      MPI_Recv(r_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &reqstat);
+      MPI_Send(s_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+    }
   }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  double t_end = MPI_Wtime();
+  cout << "time = " << t_end - t_start << endl;
 
   // if(myid == 0) {
       // double latency = (t_end - t_start) * 1e6 / (2.0 * options.loop);
